62add4de6399b1e97b694ac45b87a71eb55196da
Emma <emma.blomqvist@outlook.com>
2023-07-14 15:57:21 +0200

    import crafting recipe arrys + re-created file

--- recipes/craftingRecipes.js
+++ recipes/craftingRecipes.js
@@ -1,2 +1,1 @@
-import { refinedMaterials,enchantments,paving,fences,chests,stoves,mannequins,companions,functionalItems,furniture,balloons,touchOfMagic,fallEvent,winterEvent,springEvent,floral,lamps,signs,mailboxes } from "./crafting";
-
+import { refinedMaterials,enchantments,paving,fences,chests,stoves,mannequins,companions,functionalItems,furniture,balloons,touchOfMagic,fallEvent,winterEvent,springEvent,floral,lamps,signs,mailboxes } from "./crafting.js";
