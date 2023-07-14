e51baf47c11abcecc22043563e1fb2a272100e57
Emma <emma.blomqvist@outlook.com>
2023-07-14 15:50:26 +0200

    crafting recipe js added and linked

---
+++ recipes/craftingRecipes.js
@@ -0,0 +1,2 @@
+import { refinedMaterials,enchantments,paving,fences,chests,stoves,mannequins,companions,functionalItems,furniture,balloons,touchOfMagic,fallEvent,winterEvent,springEvent,floral,lamps,signs,mailboxes } from "./crafting";
+
