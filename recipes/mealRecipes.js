15d7a56e73b0d8c1536ff3550a93ff6db86b258a
Emma <emma.blomqvist@outlook.com>
2023-07-18 19:00:15 +0200

    meal recipes printed, ingredients WRONG have to fix tmrw

--- recipes/mealRecipes.js
+++ recipes/mealRecipes.js
@@ -1,1 +1,51 @@
 import { appetizers,entrees,desserts } from "./meals.js";
+
+function printMealRecipe(category) {
+    let object;
+    let ingredients;
+    let ingredient;
+
+    // Iterate over the array
+    for (let i = 0; i < category.length; i++) {
+        object = category[i];
+
+        // Access the materials array
+        ingredients = object.ingredients;
+
+        // Iterate over the materials array
+        for (let j = 0; j < ingredients.length; j++) {
+            ingredient = ingredients[j];
+        }
+    }
+
+    //Print objects of specified category
+    category.forEach(object => {
+        const materialBox = document.createElement("div");
+        materialBox.className = "recipeBox";
+        let specifiedCategory = document.querySelector(".appetizers");
+
+        if (category == entrees) {
+            specifiedCategory = document.querySelector(".entrees");
+        } else if (category == desserts) {
+            specifiedCategory = document.querySelector(".desserts");
+        } 
+            
+        const name = document.createElement("h3");
+        const materialName = document.createElement("p");
+        const materialAmount = document.createElement("p");
+        const sellPrice = document.createElement("p");
+        const img = document.createElement("img")
+        img.src = object.imgSrc;
+    
+        name.innerText = object.name;
+        sellPrice.innerText = object.sellPrice;
+        img.innerText = object.img;
+        materialBox.append(name,`${ingredient.name}`*length,img);
+            
+        specifiedCategory.appendChild(materialBox)
+    })
+}
+
+printMealRecipe(appetizers);
+printMealRecipe(entrees);
+printMealRecipe(desserts);
