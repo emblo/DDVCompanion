a4039fbbfcbafeef7b8d9b1f531be63c4ae7e5f0
Emma <emma.blomqvist@outlook.com>
2023-07-21 13:41:55 +0200

    meal recipes probably fixed?

--- recipes/mealRecipes.js
+++ recipes/mealRecipes.js
@@ -5,44 +5,60 @@ function printMealRecipe(category) {
     let ingredients;
     let ingredient;
 
-    // Iterate over the array
-    for (let i = 0; i < category.length; i++) {
-        object = category[i];
+    // Print objects of specified category
+    category.forEach((currentObject) => {
+        object = currentObject;
 
-        // Access the materials array
+        // Access the ingredients array inside of the current object
         ingredients = object.ingredients;
 
-        // Iterate over the materials array
-        for (let j = 0; j < ingredients.length; j++) {
-            ingredient = ingredients[j];
-        }
-    }
-
-    //Print objects of specified category
-    category.forEach(object => {
-        const materialBox = document.createElement("div");
-        materialBox.className = "recipeBox";
-        let specifiedCategory = document.querySelector(".appetizers");
-
-        if (category == entrees) {
-            specifiedCategory = document.querySelector(".entrees");
-        } else if (category == desserts) {
-            specifiedCategory = document.querySelector(".desserts");
-        } 
+        // Create a container for the current object's details
+        const objectBox = document.createElement("div");
+        objectBox.className = "recipeBox";
 
         const name = document.createElement("h3");
-
-        const sellPrice = document.createElement("p");
-        const img = document.createElement("img")
+        const img = document.createElement("img");
         img.src = object.imgSrc;
-    
+
         name.innerText = object.name;
-        sellPrice.innerText = object.sellPrice;
-        img.innerText = object.img;
-        materialBox.append(name,`${ingredient.name}`*length,img);
-            
-        specifiedCategory.appendChild(materialBox)
-    })
+        img.alt = object.name;
+        
+        // Append the name and image of the current object to its container
+        objectBox.append(name, img);
+
+        // Create a container for the ingredients of the current object
+        const ingredientsBox = document.createElement("div");
+        ingredientsBox.className = "recipeMaterials";
+
+        // Iterate over the ingredients array of the current object
+        for (let j = 0; j < ingredients.length; j++) {
+            ingredient = ingredients[j];
+
+            // Create elements for ingredient details
+            const ingredientName = document.createElement("p");
+
+            ingredientName.innerText = ingredient.name;
+
+            // Append ingredient details to the ingredients container
+            ingredientsBox.append(ingredientName);
+        }
+
+        // Append the ingredients container to the object container
+        objectBox.appendChild(ingredientsBox);
+
+        // Find the specified category container
+        let specifiedCategory;
+        if (category == appetizers) {
+            specifiedCategory = document.querySelector(".appetizers");
+        } else if (category == entrees) {
+            specifiedCategory = document.querySelector(".entrees");
+        } else {
+            specifiedCategory = document.querySelector(".desserts");
+        }
+
+        // Append the object container to the specified category container
+        specifiedCategory.appendChild(objectBox);
+    });
 }
 
 printMealRecipe(appetizers);
