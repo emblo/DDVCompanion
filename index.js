9163dcfee9a70ba32379ddbef544d82a095f8a70
Emma <emma.blomqvist@outlook.com>
2023-08-11 15:12:25 +0200

    nav menu fixed

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-//Current year for footer
-window.onload = function() {
-    let date = new Date().getFullYear();
-    document.getElementById("year").innerHTML = date;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    //Current year for footer
+    window.onload = function() {
+        let date = new Date().getFullYear();
+        document.getElementById("year").innerHTML = date;
+    } 
+
+//Nav menu toggle
+    const menuButton = document.getElementById("navButton");
+    const navMenu = document.getElementById("navMenu");
+
+    menuButton.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+    });
+})
