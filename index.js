f2cca748409edd2fbb959dd9accbc576c986f614
Emma <emma.blomqvist@outlook.com>
2023-08-11 16:35:18 +0200

    back to top button

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById("year").innerHTML = date;
     } 
 
-//Nav menu toggle
+    //Nav menu toggle
     const menuButton = document.getElementById("navButton");
     const navMenu = document.getElementById("navMenu");
 
     menuButton.addEventListener("click", () => {
         navMenu.classList.toggle("show");
     });
+
+    //Back to top button
+    const topBtn = document.getElementById("topBtn");
+    window.onscroll = function() {topBtnToggle()};
+
+    function topBtnToggle() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            topBtn.style.display = "block";
+        } else {
+            topBtn.style.display = "none";
+        }
+    }
 })
+
